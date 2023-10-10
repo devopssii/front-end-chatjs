7df608d2bd3b76d21f94e84e2c5d962fe61d7164
devopssii <133768745+devopssii@users.noreply.github.com>
2023-10-10 23:17:18 +0500

    Update app.js

--- example/app.js
+++ example/app.js
@@ -3,7 +3,7 @@ const chatContent = document.querySelector('.chatbox__support');
 const icons = {
     isClicked: '<img src="./images/icons/chatbox-icon.svg" />',
     isNotClicked: '<img src="./images/icons/chatbox-icon.svg" />'
-}
+};
 const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
 chatbox.display();
 chatbox.toggleIcon(false, chatButton);
