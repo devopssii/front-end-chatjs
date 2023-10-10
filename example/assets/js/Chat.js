6cddd36532553d53998027fc0946e338ad56e663
devopssii <133768745+devopssii@users.noreply.github.com>
2023-10-10 23:18:22 +0500

    Update Chat.js

--- example/assets/js/Chat.js
+++ example/assets/js/Chat.js
@@ -1,42 +1,83 @@
 class InteractiveChatbox {
-    constructor(a, b, c) {
-        this.args = {
-            button: a,
-            chatbox: b
-        }
-        this.icons = c;
+    constructor(button, chatbox, icons) {
+        this.button = button;
+        this.chatbox = chatbox;
+        this.icons = icons;
         this.state = false; 
+        this.messagesContainer = document.querySelector('.chatbox__messages');
+        this.inputField = document.querySelector('.chatbox__input');
+
+        this.display();
+        this.setupFormListener();
+    }
+
+    async sendMessage(message) {
+        const response = await fetch('http://api.synlabs.pro/send_message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data.response;
+        } else {
+            console.error('Error:', response.statusText);
+            return null;
+        }
     }
 
     display() {
-        const {button, chatbox} = this.args;
-        
-        button.addEventListener('click', () => this.toggleState(chatbox))
+        this.button.addEventListener('click', () => this.toggleState());
     }
 
-    toggleState(chatbox) {
+    setupFormListener() {
+        const form = document.querySelector('.chatbox__input-form');
+        form.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const userMessage = this.inputField.value;
+            this.addMessage('You', userMessage);
+            this.inputField.value = '';
+
+            const response = await this.sendMessage(userMessage);
+            if(response) {
+                this.addMessage('AI', response);
+            }
+        });
+    }
+
+    toggleState() {
         this.state = !this.state;
-        this.showOrHideChatBox(chatbox, this.args.button);
+        this.showOrHideChatBox();
     }
 
-    showOrHideChatBox(chatbox, button) {
+    showOrHideChatBox() {
         if(this.state) {
-            chatbox.classList.add('chatbox--active')
-            this.toggleIcon(true, button);
-        } else if (!this.state) {
-            chatbox.classList.remove('chatbox--active')
-            this.toggleIcon(false, button);
+            this.chatbox.classList.add('chatbox--active');
+            this.toggleIcon(true);
+        } else {
+            this.chatbox.classList.remove('chatbox--active');
+            this.toggleIcon(false);
         }
     }
 
-    toggleIcon(state, button) {
+    toggleIcon(state) {
         const { isClicked, isNotClicked } = this.icons;
-        let b = button.children[0].innerHTML;
+        const icon = this.button.children[0];
 
         if(state) {
-            button.children[0].innerHTML = isClicked; 
-        } else if(!state) {
-            button.children[0].innerHTML = isNotClicked;
+            icon.innerHTML = isClicked; 
+        } else {
+            icon.innerHTML = isNotClicked;
         }
+    }
+
+    addMessage(sender, message) {
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('message');
+        messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+        this.messagesContainer.appendChild(messageElement);
     }
 }
